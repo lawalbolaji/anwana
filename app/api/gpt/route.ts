805ba31be3391a71cf4350c8a061cefce74a4e85
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-04 20:42:27 +0100

    feat: add api route for audio file upload from client

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -1,15 +1,46 @@
 import OpenAI from "openai";
+import { NextResponse } from "next/server";
+import { Readable } from "node:stream";
+import type { ReadableStream } from "node:stream/web";
+import fs from "node:fs";
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function POST(req: Request) {
-    // const { messages } = await req.json();
-    // const response = await openai.chat.completions.create({
-    //     model: "gpt-3.5-turbo",
-    //     messages,
-    // });
+    try {
+        const formData = await req.formData();
+        const audio = formData.get("audio_blob");
+        if (audio instanceof Blob) {
+            const stream = audio.stream();
 
-    return Response.json({ message: "ok" });
+            const ws = fs.createWriteStream("uploads/audio.webm");
+            Readable.fromWeb(stream as ReadableStream<any>).pipe(ws);
+
+            await new Promise((res, rej) => {
+                ws.on("finish", () => {
+                    res("done");
+                });
+
+                ws.on("error", (err) => {
+                    rej(err);
+                });
+            });
+
+            const transcription = await openai.audio.transcriptions.create({
+                // file: fs.createReadStream("uploads/audio.webm"),
+                file: fs.createReadStream("uploads/audio.webm"),
+                model: "whisper-1",
+                response_format: "text",
+            });
+
+            console.log(transcription);
+        }
+
+        return NextResponse.json({ message: "audio saved!" });
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ message: "error" }, { status: 500 });
+    }
 }
