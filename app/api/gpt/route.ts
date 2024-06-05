8d59585ebcc5bdcd67b42d00a9c105dbcc80bef3
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-05 11:32:50 +0100

    feat: add route handler for gpt audio from client

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -2,7 +2,7 @@ import OpenAI from "openai";
 import { NextResponse } from "next/server";
 import { Readable } from "node:stream";
 import type { ReadableStream } from "node:stream/web";
-import fs from "node:fs";
+import { toFile } from "openai/uploads";
 
 const openai = new OpenAI({
     apiKey: process.env.OPENAI_API_KEY,
@@ -15,30 +15,47 @@ export async function POST(req: Request) {
         if (audio instanceof Blob) {
             const stream = audio.stream();
 
-            const ws = fs.createWriteStream("uploads/audio.webm");
-            Readable.fromWeb(stream as ReadableStream<any>).pipe(ws);
-
-            await new Promise((res, rej) => {
-                ws.on("finish", () => {
-                    res("done");
-                });
-
-                ws.on("error", (err) => {
-                    rej(err);
-                });
-            });
-
-            const transcription = await openai.audio.transcriptions.create({
-                // file: fs.createReadStream("uploads/audio.webm"),
-                file: fs.createReadStream("uploads/audio.webm"),
+            /* This utility from openAI allows us to convert the stream to a compatible file format without having to save the audio to disk and creating a readstream with fs.createReadStream() */
+            const byteStreamAsFileLike = await toFile(Readable.fromWeb(stream as ReadableStream<any>), "audio.webm");
+            const transcriptionAsText = await openai.audio.transcriptions.create({
+                file: byteStreamAsFileLike,
                 model: "whisper-1",
                 response_format: "text",
             });
 
-            console.log(transcription);
-        }
+            // get completions
+            const completions = await openai.chat.completions.create({
+                model: "gpt-4o",
+                messages: [
+                    {
+                        role: "system",
+                        content:
+                            "You are a helpful assistant that helps me general knowledge questions. You want to prioritize the shortest answers but in any case, you answer must not exceed 20 words",
+                    },
+                    { role: "user", content: transcriptionAsText + "" },
+                ],
+            });
 
-        return NextResponse.json({ message: "audio saved!" });
+            /* get text-to-speech model */
+            const response = await openai.audio.speech.create({
+                model: "tts-1",
+                voice: "shimmer",
+                input: completions.choices[0].message.content ?? "I didn't catch that, please come again",
+            });
+
+            const blob = await response.arrayBuffer();
+            const buffer = Buffer.from(blob);
+            const contentType = response.headers.get("content-type") || "application/octet-stream";
+            const contentLength = response.headers.get("content-length") || blob.byteLength;
+
+            return new NextResponse(buffer, {
+                status: 200,
+                headers: {
+                    "content-type": contentType,
+                    "content-length": contentLength + "",
+                },
+            });
+        }
     } catch (error) {
         console.log(error);
         return NextResponse.json({ message: "error" }, { status: 500 });
