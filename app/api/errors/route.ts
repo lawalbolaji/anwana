a320cb74e9c9e8e26c267443972e876d2e273861
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-12 23:35:39 +0100

    fix: actually log remote errors

--- app/api/errors/route.ts
+++ app/api/errors/route.ts
@@ -1,3 +1,10 @@
+import { NextResponse } from "next/server";
+import { stdout } from "node:process";
+import { Readable } from "node:stream";
+import { ReadableStream } from "node:stream/web";
+
 export async function POST(req: Request) {
-    console.log(req.body);
+    /* log error payload to console */
+    Readable.fromWeb(req.body as ReadableStream<any>).pipe(stdout);
+    return NextResponse.json("ok");
 }
