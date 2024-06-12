e50c02a0c6b414e37050ef0d2015f56622c9a974
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-06-12 19:36:22 +0100

    style: add shadow presets to tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,24 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        extend: {
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+            boxShadow: {
+                I: "3px 3px 3px rgba(0,0,0, 0.3), -3px -3px 3px rgba(255,255,255, 0.3), 3px -3px 3px rgba(0,0,0, 0.3), -3px 3px 3px rgba(255,255,255, 0.3)",
+                II: "6px 6px 6px rgba(0,0,0, 0.5), -6px -6px 6px rgba(255,255,255, 0.5), 6px -6px 6px rgba(0,0,0, 0.5), -6px 6px 6px rgba(255,255,255, 0.5)",
+                III: "6px 6px 6px rgba(228, 230, 234, 0.5), -6px -6px 6px rgba(228, 230, 234, 0.5), 6px -6px 6px rgba(228, 230, 234, 0.5), -6px 6px 6px rgba(228, 230, 234, 0.5)",
+            },
+        },
     },
-  },
-  plugins: [require("tailwindcss-animate")],
+    plugins: [require("tailwindcss-animate")],
 };
 export default config;
