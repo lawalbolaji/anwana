d44a2754f7f6c3b4f747ec504659018eaf232bf0
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-07-24 11:43:48 +0100

    feat: add deepgram and groq clients for alternative stt/tts

--- app/api/gpt/route.ts
+++ app/api/gpt/route.ts
@@ -1,33 +1,57 @@
 import OpenAI from "openai";
+import { createClient } from "@deepgram/sdk";
 import { NextResponse } from "next/server";
 import { Readable } from "node:stream";
 import type { ReadableStream } from "node:stream/web";
 import { toFile } from "openai/uploads";
-
-const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY,
-});
+import Groq from "groq-sdk";
 
 export const maxDuration = 20;
 
-export async function POST(req: Request) {
-    try {
-        const formData = await req.formData();
-        const audio = formData.get("audio_blob");
-        const mimeType = formData.get("type");
-        const fileExt =
-            mimeType === "audio/wav"
-                ? "wav"
-                : mimeType === "audio/webm"
-                ? "webm"
-                : mimeType === "audio/ogg"
-                ? "ogg"
-                : "mp4";
-        const filename = `request.${fileExt}`;
+const deepgramClient = (function () {
+    const deepgram = createClient(process.env.DEEPGRAM_API_KEY);
+    return Object.freeze({
+        async stt(stream: globalThis.ReadableStream<Uint8Array>) {
+            const { result, error } = await deepgram.listen.prerecorded.transcribeFile(
+                Readable.fromWeb(stream as ReadableStream<any>),
+                { model: "nova-2" }
+            );
 
-        if (audio instanceof Blob) {
-            const stream = audio.stream();
+            if (error) throw error;
+            return result?.results.channels?.[0].alternatives?.[0].transcript;
+        },
+        async tts(speech: string) {},
+    });
+})();
 
+const grokClient = (function () {
+    const groq = new Groq({ apiKey: process.env.GROQ_API_KEY });
+    const filename = `request.wav`;
+    return Object.freeze({
+        async stt(stream: globalThis.ReadableStream<Uint8Array>) {
+            const transcription = await groq.audio.transcriptions.create({
+                file: await toFile(Readable.fromWeb(stream as ReadableStream<any>), filename),
+                model: "whisper-large-v3",
+                prompt: "Specify context or spelling", // Optional
+                response_format: "json", // Optional
+                language: "en", // Optional
+                temperature: 0.0, // Optional
+            });
+
+            return transcription.text;
+        },
+        async tts(speech: string) {},
+    });
+})();
+
+const openaiClient = (function () {
+    const openai = new OpenAI({
+        apiKey: process.env.OPENAI_API_KEY,
+    });
+
+    const filename = `request.wav`;
+    return Object.freeze({
+        async stt(stream: globalThis.ReadableStream<Uint8Array>) {
             /* This utility from openAI allows us to convert the stream to a compatible file format without having to save the audio to disk and creating a readstream with fs.createReadStream() */
             const byteStreamAsFileLike = await toFile(Readable.fromWeb(stream as ReadableStream<any>), filename);
             const transcriptionAsText = await openai.audio.transcriptions.create({
@@ -37,9 +61,19 @@ export async function POST(req: Request) {
                 language: "en",
             });
 
-            console.log(`transcription request complete`);
+            return transcriptionAsText as unknown as string;
+        },
+        async tts(speech: string) {
+            const response = await openai.audio.speech.create({
+                model: "tts-1",
+                voice: "shimmer",
+                input: speech,
+                speed: 0.8,
+            });
 
-            // get completions
+            return response;
+        },
+        async getCompletion(userPrompt: string) {
             const completions = await openai.chat.completions.create({
                 model: "gpt-4o-mini",
                 messages: [
@@ -48,20 +82,30 @@ export async function POST(req: Request) {
                         content:
                             "You are a helpful assistant that helps with general knowledge questions about Nigeria. You want to prioritize the shortest answers but in any case, you answer must not exceed 30 words. Generate all your responses in Nigerian pidjin english!",
                     },
-                    { role: "user", content: transcriptionAsText + "" },
+                    { role: "user", content: userPrompt + "" },
                 ],
             });
 
+            return completions.choices[0].message.content;
+        },
+    });
+})();
+
+export async function POST(req: Request) {
+    try {
+        const formData = await req.formData();
+        const audio = formData.get("audio_blob");
+        if (audio instanceof Blob) {
+            const stream = audio.stream();
+
+            // const transcriptionAsText = await deepgramClient.stt(stream);
+            const transcriptionAsText = await openaiClient.stt(stream);
+            // const transcriptionAsText = await grokClient.stt(stream);
+
+            console.log(`transcription request complete`);
+            const speech = await openaiClient.getCompletion(transcriptionAsText);
             console.log(`completion request complete`);
-
-            /* get text-to-speech model */
-            const response = await openai.audio.speech.create({
-                model: "tts-1",
-                voice: "shimmer",
-                input: completions.choices[0].message.content ?? "I didn't catch that, please come again",
-                speed: 0.8,
-            });
-
+            const response = await openaiClient.tts(speech ?? "I didn't catch that, please come again");
             console.log(`tts request complete`);
 
             const blob = await response.arrayBuffer();
