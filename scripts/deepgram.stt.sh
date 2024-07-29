e62404f7a2b524f52c8011e05d1a34ab7e65c96e
abdulrasheedlawal <lawalabdulrasheedbolaji@gmail.com>
2024-07-29 16:29:11 +0100

    fix deepgram testing script

--- scripts/deepgram.stt.sh
+++ scripts/deepgram.stt.sh
@@ -1,6 +1,5 @@
 curl -w "@./scripts/curl_format.txt" -so /dev/null --request POST \
-     --header "Content-Type: application/json" \
+     --header "Content-Type: audio/wav" \
      --header "Authorization: Token ${DEEPGRAM_API_KEY}" \
-     --output your_output_file.mp3 \
-     --data '{"text":"Hello, how can I help you today?"}' \
-     --url "https://api.deepgram.com/v1/speak?model=aura-asteria-en"
+     --data-binary @./public/introduction.mp3 \
+     --url 'https://api.deepgram.com/v1/listen?model=nova-2&smart_format=true'
